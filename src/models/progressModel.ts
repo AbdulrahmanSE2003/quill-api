4cc057e2fa307c1b221df91bd708ce18eb870c28
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-07-31 00:53:03 +0300

    feat: Progress model

---
+++ src/models/progressModel.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface IProgress extends Document {
+  userId: Types.ObjectId;
+  bookId: Types.ObjectId;
+  status: "not_started" | "reading" | "completed";
+  currentChunkIndex: number; //(default: 0)
+  isFavorite: boolean;
+  lastReadAt: Date;
+}
+
+const progressSchema = new Schema<IProgress>({
+  userId: { type: Schema.ObjectId, ref: "User", required: true },
+  bookId: { type: Schema.ObjectId, ref: "Book", required: true },
+  status: { type: String, enum: ["not_started", "reading", "completed"] },
+  currentChunkIndex: { type: Number, required: true, default: 0 },
+  isFavorite: Boolean,
+  lastReadAt: Date,
+});
+
+progressSchema.index({ userId: 1, bookId: 1 });
+
+const Progress = mongoose.model<IProgress>("Progress", progressSchema);
+
+export default Progress;
