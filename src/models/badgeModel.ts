73df6e8392033fea4af6a129208a76f9a75f1bd3
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-07-31 01:33:30 +0300

    feat: FInished models

--- src/models/badgeModel.ts
+++ src/models/badgeModel.ts
@@ -2,14 +2,14 @@ import mongoose, { Document, Schema, Types } from "mongoose";
 
 export interface IBadge extends Document {
   userId: Types.ObjectId;
-  badges: "" | ""; //TODO handle later
+  type: "" | ""; //TODO handle later
   earnedAt: Date;
 }
 
 const badgeSchema = new Schema<IBadge>({
   userId: { type: Schema.ObjectId, ref: "User", required: true },
-  badges: { type: String, enum: [""] },
-  earnedAt: Date,
+  type: { type: String, enum: [""] },
+  earnedAt: { type: Date, default: Date.now },
 });
 
 const Badge = mongoose.model<IBadge>("Badge", badgeSchema);
